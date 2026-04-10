e8f32c68afaaf5ad50025f56a2202a36014da945
L-Airennn <muhammadmumin082@gmail.com>
2026-04-11 01:11:19 +0700

    make Middleware folder on app /http/ and then add CheckRole.php

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Periksa apakah user yang login memiliki role yang diizinkan.
+     *
+     * Penggunaan di route: middleware('role:admin') atau middleware('role:karyawan')
+     */
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        $userRole = Auth::user()->role;
+
+        if (!in_array($userRole, $roles)) {
+            abort(403, 'Akses ditolak. Anda tidak memiliki izin untuk halaman ini.');
+        }
+
+        return $next($request);
+    }
+}
